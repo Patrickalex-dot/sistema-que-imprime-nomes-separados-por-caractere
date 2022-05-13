10596501031b910cc34a8c15fded36e66fcda069
patrick alexandre <palexandre@live.com>
2022-05-12 21:51:35 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace sistema_que_imprime_nomes_separados_por_caractere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Nomes = new string[10];
+
+            for (int i = 0; i < Nomes.Length; i++)
+            {
+                Console.WriteLine("digite 10 nomes");
+                Nomes[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < Nomes.Length; i++)
+            {
+                Console.Write($"{Nomes[i]}");
+                Console.Write($"-OU-");
+            }
+        }
+    }
+}
